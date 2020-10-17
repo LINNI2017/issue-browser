f5b3274d449f97185be9b2042359562077da1ecc
LINNI CAI <cai20170607@gmail.com>
2020-10-17 09:56:31 -0700

    add docs

---
+++ docs/precache-manifest.3476c8f78238d48db7261265abc9fdd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a60f30d1355b6b9477fd0bd797a5ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf3de7635eb6af52b792",
+    "url": "/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "15dc02940e84fa8ff55f",
+    "url": "/static/css/main.c101999c.chunk.css"
+  },
+  {
+    "revision": "cf3de7635eb6af52b792",
+    "url": "/static/js/2.2f7bd380.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.2f7bd380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15dc02940e84fa8ff55f",
+    "url": "/static/js/main.ae6071c8.chunk.js"
+  },
+  {
+    "revision": "fa75dc5fd21fddb5d707",
+    "url": "/static/js/runtime-main.fd78dc8a.js"
+  }
+]);
